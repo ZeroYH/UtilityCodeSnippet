1a45b932f70bc2bb2752ae701b6123ead561715e
SpiderMan <495419587@qq.com>
2018-08-06 16:18:35 +0800

    s

---
+++ DeviceSpecificModel.h
@@ -0,0 +1,20 @@
+//
+//  DeviceSpecificModel.h
+//  Zone
+//
+//  Created by SpiderMan on 2018/8/6.
+//  Copyright © 2018年 pansoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DeviceSpecificModel : NSObject
+
+/**
+ 具体的设备型号
+
+ @return 设备型号
+ */
++ (NSString*)deviceSpecificModelName;
+
+@end
